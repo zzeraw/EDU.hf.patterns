63500f7a87fcf8351222ffc7f0943b365ec38bac
zzeraw <pekidoch@gmail.com>
2013-01-31 23:15:46 +0400

    no message

--- 01-Strategy/index.php
+++ 01-Strategy/index.php
@@ -1,8 +1,8 @@
 <?php
 
 abstract class Duck {
-    public $flyBehavior; // свойство типа FlyBehavior, ссылка на класс
-    public $quackBehavior; // свойство типа QuackBehavior, ссылка на класс
+    public $flyBehavior; // каждый объект Duck содержит ссылку на реализацию интерфейса FlyBehavior
+    public $quackBehavior; // каждый объект Duck содержит ссылку на реализацию интерфейса QuackBehavior
 
     function __construct() {
 
@@ -11,11 +11,11 @@ abstract class Duck {
     public abstract function display();
 
     public function performFly() {
-        $this->flyBehavior->fly();
+        $this->flyBehavior->fly(); // объект Duck делегирует ПОВЕДЕНИЕ объекту, на который ссылается flyBehavior.
     }
 
     public function performQuack() {
-        $this->quackBehavior->quack();
+        $this->quackBehavior->quack(); // объект Duck делегирует ПОВЕДЕНИЕ объекту, на который ссылается quackBehavior.
     }
 
     public function swim() {
@@ -31,9 +31,10 @@ abstract class Duck {
 }
 
 class MallardDuck extends Duck {
+
     function __construct() {
-        $this->quackBehavior  = new Quack();
-        $this->flyBehavior    = new FlyWithWings();
+        $this->quackBehavior  = new Quack(); // MallardDuck использует класс Quack для выполнения действия, так что при вызове performQuack() отвественность за выполнение возлагается на объект Quack.
+        $this->flyBehavior    = new FlyWithWings(); // А в качестве реализации FlyBehavior используется тип FlyWithWings.
     }
 
     public function display() {
@@ -44,7 +45,7 @@ class MallardDuck extends Duck {
 class ModelDuck extends Duck {
     function __construct() {
         $this->quackBehavior  = new Quack();
-        $this->flyBehavior    = new FlyNoWay();
+        $this->flyBehavior    = new FlyNoWay(); // утка-приманка изначально не умеет летать
     }
 
     public function display() {
@@ -52,24 +53,27 @@ class ModelDuck extends Duck {
     }
 }
 
-
+// интерфейс реализуется всеми классами
 interface FlyBehavior {
     public function fly();
 }
 
 class FlyWithWings implements FlyBehavior {
+    // Реализация поведения для уток, которые УМЕЮТ летать
     public function fly() {
         echo "<div>I'm flying!</div>";
     }
 }
 
 class FlyNoWay implements FlyBehavior {
+    // Реализация поведения для утрок, которые НЕ ЛЕТАЮТ (например, резиновых)
     public function fly() {
         echo "<div>I can't fly!</div>";
     }
 }
 
 class FlyRockedPowered implements FlyBehavior {
+    // определяем новое поведение - реактивный полет
     public function fly() {
         echo "<div>I'm flying with a rocked!</div>";
     }
@@ -97,13 +101,13 @@ class Squeak implements quackBehavior{
 class MiniDucksSimulator {
     public static function main($args = NULL) {
         $mallard = new MallardDuck();
-        $mallard->performQuack();
-        $mallard->performFly();
+        $mallard->performQuack(); // вызов метода performQuack(), унаследованного классом MallardDuck; метод делегирует выполнение операции по ссылке на QuackBehavior (то есть вызывает quack() через унаследованную переменную quackBehavior() ).
+        $mallard->performFly(); // затем то же самое происходит с методом performFly(), также унаследованным классов MallardDuck.
 
         $model = new ModelDuck();
-        $model->performFly();
-        $model->setFlyBehavior(new FlyRockedPowered);
-        $model->performFly();
+        $model->performFly(); // первый вызов performFly() передается реализации, заданной в конструкторе ModelDuck - то есть экземпляру FlyNoWay.
+        $model->setFlyBehavior(new FlyRockedPowered); // Вызываем set-метод, унаследованный классом ModelDuck, и... утка-приманка вдруг взлетает на реактивном двигателе
+        $model->performFly(); // способность утки-приманки к полету переключается динамически! Если бы реализация находилась в иерархии  Duck, такое было бы невозможно.
     }
 }
 
